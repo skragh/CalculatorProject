3153e7d2168f7ed663b63ad0970940077139bfab
skragh <83223908+skragh@users.noreply.github.com>
2021-09-02 11:52:20 +0200

    Divide funktion implemented and tested

--- Calculator.Unit.Test/CalculatorUnitTest.cs
+++ Calculator.Unit.Test/CalculatorUnitTest.cs
@@ -46,5 +46,13 @@ namespace Calculator.Unit.Test
             double result = uut.Subtract(-4.3, 3.7);
             Assert.True(result==-8.0);
         }
+
+        //dividorTest
+        [Test]
+        public void Divide_PositiveFromPositive_AssertResultCorrect()
+        {
+            double result = uut.Divide(9, 3);
+            Assert.That(result, Is.EqualTo(3));
+        }
     }
 }
--- CalculatorProject/Calculator.cs
+++ CalculatorProject/Calculator.cs
@@ -27,5 +27,10 @@ namespace CalculatorProject
         {
         return Math.Pow(x,exp);
         }
+
+        public double Divide(double dividend, double divisor)
+        {
+            return dividend / divisor;
+        }
     }
 }
