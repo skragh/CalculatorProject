a0531dbefd6eb46a1350935bff72a8961b587608
skragh <83223908+skragh@users.noreply.github.com>
2021-09-07 09:37:30 +0200

    Added tests for Calculator.Multiply and Calculator.Power

--- Calculator.Unit.Test/CalculatorUnitTest.cs
+++ Calculator.Unit.Test/CalculatorUnitTest.cs
@@ -48,10 +48,34 @@ namespace Calculator.Unit.Test
         }
 
         //Multiply
+        [Test]
+        public void Multiply_PositiveWithPositive_AssertValueCorrect()
+        {
+            double result = uut.Multiply(4, 5.5);
+            Assert.That(result, Is.EqualTo(22.0).Within(0.00001));
+        }
 
-        //Exponent
+        public void Multiply_PositiveWithNegative_AssertValueCorrect()
+        {
+            double result = uut.Multiply(2.5, -6);
+            Assert.That(result, Is.EqualTo(-22.0).Within(0.00001));
+        }
 
-        //dividorTest
+        public void Multiply_NegativeWithNegative_AssertValueCorrect()
+        {
+            double result = uut.Multiply(-30, -1.2);
+            Assert.That(result, Is.EqualTo(35.0).Within(0.00001));
+        }
+
+        //Power
+
+        public void Power_PositiveWithPositive_AssertValueCorrect()
+        {
+            double result = uut.Power(5, 2);
+            Assert.That(result, Is.EqualTo(25).Within(0.0001));
+        }
+
+        //divideTest
         [Test]
         public void Divide_PositiveFromPositive_AssertResultCorrect()
         {
